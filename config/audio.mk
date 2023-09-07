fac50a1bd32f8c02f92f2634464e7cb05b61b8ca
lahaina <nattolecats@gmail.com>
2023-09-08 01:46:05 +0900

    config: audio: Add more audio

--- config/audio.mk
+++ config/audio.mk
@@ -1,22 +1,39 @@
 LOCAL_PATH := vendor/auroraui/audio
 LOCAL_PATH_FWB := frameworks/base/data/sounds
 
+ALARM_FILES := Argon Carbon Helium Krypton Neon Oxygen Osmium Platinum Timer
+NOTIFICATION_FILES := Ariel Ceres Carme Elara Europa Iapetus Io Rhea Salacia Titan Tethys
+RINGTONE_FILES := Atria Callisto Dione Ganymede Luna Oberon Phobos Pyxis Sedna Titania Triton \
+	Umbriel
+EFFECT_FILES := Dock InCallNotification KeypressInvalid Lock LowBattery \
+    NFCFailure NFCInitiated NFCSuccess NFCTransferComplete NFCTransferInitiated \
+    Trusted Undock Unlock
+
 # Alarms
+
+PRODUCT_COPY_FILES += $(foreach fn,$(ALARM_FILES),\
+    $(LOCAL_PATH_FWB)/alarms/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/$(fn).ogg)
+
 $(foreach f, $(wildcard $(LOCAL_PATH)/alarms/*.m4a), \
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/$(notdir $f)))
 
 # Ringtones
+
+PRODUCT_COPY_FILES += $(foreach fn,$(RINGTONE_FILES),\
+    $(LOCAL_PATH_FWB)/ringtones/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/$(fn).ogg)
+
 $(foreach f, $(wildcard $(LOCAL_PATH)/ringtones/*.m4a), \
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/$(notdir $f)))
 
 # Notifications
+
+PRODUCT_COPY_FILES += $(foreach fn,$(NOTIFICATION_FILES),\
+    $(LOCAL_PATH_FWB)/notifications/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/$(fn).ogg)
+
 $(foreach f, $(wildcard $(LOCAL_PATH)/notifications/*.m4a), \
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/$(notdir $f)))
 
 # UI
-EFFECT_FILES := Dock InCallNotification KeypressInvalid Lock LowBattery \
-    NFCFailure NFCInitiated NFCSuccess NFCTransferComplete NFCTransferInitiated \
-    Trusted Undock Unlock
 
 PRODUCT_COPY_FILES += $(foreach fn,$(EFFECT_FILES),\
     $(LOCAL_PATH_FWB)/effects/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/$(fn).ogg)
